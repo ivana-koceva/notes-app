4c2aaf9ebc262404136c9175ac8af779248469a3
Ivana Koceva <ivana.koceva10@gmail.com>
2024-06-20 18:30:25 +0200

    added service and interface

--- backend/src/main/java/com/notes/notesapplication/NotesApplication.java
+++ backend/src/main/java/com/notes/notesapplication/NotesApplication.java
@@ -2,6 +2,12 @@ package com.notes.notesapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @SpringBootApplication
 public class NotesApplication {
@@ -9,5 +15,17 @@ public class NotesApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotesApplication.class, args);
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of("http://localhost:4200"));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        corsConfiguration.setAllowedHeaders(List.of("*"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+    }
+
 
 }
